1754af10df9fa0d8b8a1a6df1644a68dfe4b4922
leeseooo <leeseoooo@naver.com>
2022-11-13 10:20:28 +0900

    chore: 초기세팅 수정

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
- module.exports = {
-    "printWidth": 100, // 줄 바꿈할 줄 길이
-    "tabWidth": 2, // 들여쓰기 칸 수
-    "semi": true, // 세미콜론 강제
-    "singleQuote": true, // 작은 따옴표 사용을 강제
-    "trailingComma": "all", // 쉼표로 구분된 여러 줄인 구문에서 후행 쉼표를 추가
-    "endOfLine": "auto" // EoF 방식, OS별로 처리 방식이 다름
-  }
