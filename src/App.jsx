783c6c5656c799e5188708f8de882f6fa42972dc
leeseooo <leeseoooo@naver.com>
2022-11-13 10:29:17 +0900

    chore: 폴더구조 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,54 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+export const GlobalStyle = createGlobalStyle`
+    ${reset}
+    :root {
+        --vh: 100%;
+    }
+    html,
+    body {
+        padding: 0;
+        margin: 0;
+        line-height: 1.6;
+        font-size: 62.5%;
+    }
+    * {
+        box-sizing: border-box;
+    }
+    
+    input,
+    button,
+    select,
+    textarea,
+    optgroup,
+    option {
+        font-family: inherit;
+        font-size: inherit;
+        font-style: inherit;
+        font-weight: inherit;
+    }
+    input[type="checkbox"] {
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        border: 1px solid black;
+        background: #fff;
+        border-radius: 4px;
+        cursor: pointer;
+        height: 16px;
+        width: 16px;
+        outline: 0;
+    }
+    input[type="checkbox"]::after {
+        display: none;
+    }
+    input[type="checkbox"]:checked {
+        background: #FF307B;
+        border: 1px solid #FF307B;
+        
+    }
+    input[type="checkbox"]:checked::after {
+        display: block;
+    }
+`;
