181a7c95396fb882ad7facf4a41001ddcca37c48
leeseooo <leeseoooo@naver.com>
2022-11-14 12:10:14 +0900

    feat/#1: router todo 경로 수정, 색깔 상수 추가

---
+++ src/styles/color.js
@@ -0,0 +1,9 @@
+export const pinterestColors = Object.freeze({
+  black: '#111111',
+  gray400: '#767676',
+  gray300: '#d9d9d9',
+  gray200: '#e9e9e9',
+  gray100: '#f7f7f7',
+  white: '#fff',
+  primary: '#e60023',
+});
