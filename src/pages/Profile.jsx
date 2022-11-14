c7cdd549f2e29f4ebca43ec4e186715bc9615c97
Na Rim <skfla07@naver.com>
2022-11-14 16:35:26 +0900

    feat/#8: 프로필,보드 영역 구분

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <StyledRoot>
+      <StyledProfileContainer></StyledProfileContainer>
+      <StyledBoardContainer>보드 영역 입니다</StyledBoardContainer>
+    </StyledRoot>
+  );
 }
 
 export default Profile;
+
+const StyledRoot = styled.div`
+  width: 1920px;
+  height: 963px; //header height 117px 고려한 값
+  display: flex;
+  background-color: green;
+`;
+
+const StyledProfileContainer = styled.div`
+  width: 288px;
+  height: 500px;
+  margin-left: 48px;
+  margin-top: 26px; //header height: 117px 고려한 값
+  border: 1px solid #e9e9e9;
+  border-radius: 14px;
+`;
+
+const StyledBoardContainer = styled.div`
+  width: 1584px;
+  height: 100%;
+  padding: 3rem;
+`;
