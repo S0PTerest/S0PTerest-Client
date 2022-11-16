91ab6bc529462dd229dbda28df936ea083577f30
[talkingOrange] <sonrisa-bonita@naver.com>
2022-11-16 23:00:47 +0900

    style/#9: 활성화된 사용자 아이콘 위치 세팅

--- src/components/board/PinTitle.jsx
+++ src/components/board/PinTitle.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-// import { pinterestColors } from '../../styles/color';
 import { ReactComponent as IMenu } from '../../assets/iMenu.svg';
 import { ReactComponent as IClickedMenu } from '../../assets/iClickedMenu.svg';
+import { ReactComponent as ActiveUser1 } from '../../assets/activeUser1.svg';
+import { ReactComponent as ActiveUser2 } from '../../assets/activeUser2.svg';
+import { ReactComponent as ActiveUser3 } from '../../assets/activeUser3.svg';
 
 function PinTitle() {
   const [isIconHover, setIsIconHover] = useState(false);
   return (
     <StyledRoot>
       <StyledTitle>
+        {/* 글자수 제한은 작성 시 검사가 이루어지기 때문에 후에 데이터를 받아오는 과정만 추가해주면 됨. */}
         <span>민본포_7조 제목이 길어진다면 (50자 제한이라고 함)</span>
         <span onMouseOver={() => setIsIconHover(true)} onMouseOut={() => setIsIconHover(false)}>
           {isIconHover ? <IClickedMenu /> : <IMenu />}
@@ -16,9 +19,9 @@ function PinTitle() {
       </StyledTitle>
 
       <StyledActiveUser>
-        <img class="activeUser1" src="activeUser1.svg" alt="activeUser1" />
-        <img class="activeUser2" src="activeUser2.svg" alt="activeUser2" />
-        <img class="activeUser3" src="activeUser3.svg" alt="activeUser3" />
+        <ActiveUser1 class="activeUser1" />
+        <ActiveUser2 class="activeUser2" />
+        <ActiveUser3 class="activeUser3" />
       </StyledActiveUser>
     </StyledRoot>
   );
@@ -46,11 +49,11 @@ const StyledTitle = styled.div`
 const StyledActiveUser = styled.div`
   .activeUser1 {
     position: relative;
-    right: 10px;
+    left: 3.2rem;
   }
   .activeUser2 {
-    position: absolute;
-    right: 28px;
+    position: relative;
+    left: 1.6rem;
   }
   .activeUser3 {
     position: relative;
