665a3f0c3cf98c8f0f9e6a8f1fcbe21743a75b2e
[talkingOrange] <sonrisa-bonita@naver.com>
2022-11-16 23:46:44 +0900

    style/#16: NoteList.jsx 파일 Board 화면에 합치기

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import PinTitle from '../components/board/PinTitle';
 import PinGallery from '../components/board/PinGallery';
+import NoteList from '../components/NoteList';
 
 function Board() {
   return (
-    <>
+    <StyledRoot>
       <PinTitle />
-      <PinGallery />
-    </>
+      <hr />
+      <StyledMain>
+        <NoteList />
+        <PinGallery />
+      </StyledMain>
+    </StyledRoot>
   );
 }
 
 export default Board;
+
+const StyledRoot = styled.div`
+  hr {
+    margin-top: 2.8rem;
+    margin-bottom: 3.2rem;
+  }
+`;
+
+const StyledMain = styled.main`
+  display: flex;
+`;
