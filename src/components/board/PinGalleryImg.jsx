7555a52a74f849480efecdb36ac6ab3b948df5dd
[talkingOrange] <sonrisa-bonita@naver.com>
2022-11-21 20:31:34 +0900

    feat/#16: PinGalleryImg 배열 수정

--- src/components/board/PinGalleryImg.jsx
+++ src/components/board/PinGalleryImg.jsx
@@ -2,22 +2,58 @@ import React from 'react';
 import styled from 'styled-components';
 
 function PinGalleryImg() {
-  const galleryImgs = [
+  const galleryAgenda = [
+    {
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSkg_TB2-0KHCCWMsu6zK6C6iVdxKBXzn05Q&usqp=CAU',
+      title: '제목1',
+      profile: '프로필 사진',
+      userId: '사용자',
+    },
+    {
+      img: 'https://img.insight.co.kr/static/2020/06/15/700/h4ubvce2f6vz4znnbnmw.jpg',
+      title: '제목2',
+      profile: '프로필 사진',
+      userId: '사용자',
+    },
+    {
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhFnvgVUdi16OJAXTkdTIZVPYNWMzoNp-B2Q&usqp=CAU',
+      title: '제목3',
+      profile: '프로필 사진',
+      userId: '사용자',
+    },
+    {
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE-GkQErCAJUJ95ZP-g9Bf77rmCX6Su5HX_Q&usqp=CAU',
+      title: '제목4',
+      profile: '프로필 사진',
+      userId: '사용자',
+    },
+    {
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5K2hHft7N_Wac201KXS7S_WwPSFUWm6Rnrw&usqp=CAU',
+      title: '제목5',
+      profile: '프로필 사진',
+      userId: '사용자',
+    },
+    {
+      img: 'https://mblogthumb-phinf.pstatic.net/20160408_268/chadol0000_146009711879167ocW_JPEG/google_co_kr_20160408_152734.jpg?type=w2',
+      title: '제목6',
+      profile: '프로필 사진',
+      userId: '사용자',
+    },
     // 이미지 주소 복사로 임의의 사진을 배열로 추가함. 추후 사진 데이터를 받아와야함.
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSkg_TB2-0KHCCWMsu6zK6C6iVdxKBXzn05Q&usqp=CAU',
-    'https://img.insight.co.kr/static/2020/06/15/700/h4ubvce2f6vz4znnbnmw.jpg',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhFnvgVUdi16OJAXTkdTIZVPYNWMzoNp-B2Q&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE-GkQErCAJUJ95ZP-g9Bf77rmCX6Su5HX_Q&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5K2hHft7N_Wac201KXS7S_WwPSFUWm6Rnrw&usqp=CAU',
-    'https://mblogthumb-phinf.pstatic.net/20160408_268/chadol0000_146009711879167ocW_JPEG/google_co_kr_20160408_152734.jpg?type=w2',
+    ,
   ];
 
   return (
     <StyledRoot>
-      {galleryImgs.map((galleryImg, index) => {
+      {galleryAgenda.map((galleryData, index) => {
         return (
           <StyledGalleryImg>
-            <img src={galleryImg} />
+            <img src={galleryData.img} />
+            <StyledInform>
+              {galleryData.title}
+              {galleryData.profile}
+              {galleryData.userId}
+            </StyledInform>
           </StyledGalleryImg>
         );
       })}
@@ -33,7 +69,8 @@ const StyledRoot = styled.div`
 `;
 
 const StyledGalleryImg = styled.div`
-  display: block;
+  display: flex;
+  flex-direction: column;
   margin-bottom: 3.4em;
   cursor: pointer;
 
@@ -45,3 +82,5 @@ const StyledGalleryImg = styled.div`
     width: 28.8rem;
   }
 `;
+
+const StyledInform = styled.div``;
