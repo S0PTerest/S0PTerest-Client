2aaaf16663e4658fc93e244c5e91d21a2f213e67
leeseooo <leeseoooo@naver.com>
2022-11-22 17:57:38 +0900

    chore/#21: useStatus boolean상태 변경 코드 수정

--- src/utils/hooks/useStatus.jsx
+++ src/utils/hooks/useStatus.jsx
@@ -4,13 +4,12 @@ export const useStatus = (initialValue) => {
   const [status, onChange] = useState(initialValue);
 
   const setStatus = (idx) => {
-    console.log(idx);
     let newStatus = new Array(status.length).fill(false);
     if (status.some((val) => val) && status.indexOf(true) === idx) {
       onChange(newStatus);
       return;
     }
-    newStatus[idx] = newStatus[idx] ? false : true;
+    newStatus[idx] = !newStatus[idx];
     onChange(newStatus);
   };
   return [status, setStatus];
